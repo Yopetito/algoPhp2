1de64cc8495d6e4905712bc2bdccad8e916e69cc
yopetito <yofer.frg@gmail.com>
2024-10-28 17:01:32 +0100

    exo 6 et debut du 7

--- exo6.php
+++ exo6.php
@@ -24,13 +24,11 @@ echo alimenterListeDeroulante($elements);
 function alimenterListeDeroulante($elements){
     $form = "<form action='' method='post'>"; //form=Formulaire action=Définit l'url ou les données du formulaire seront envoyées. Ici meme dans ce cas 
                                               //method=post : transmettre les données de manière sécurisée (sans les afficher dans l'URL)
-    $label = "<label for='civilites'>Choisir votre civilité</label><br>";
+    $label = "<label for='civilites'>Choisir:</label><br>";
     $sel = "<select name='civilities' id='civilites'>";
-       
     foreach ($elements as $civility){
             $sel .= "<option value='$civility'>$civility</option>"; //fait les cases de la liste deroulante attribué a $sel 
         }
-$sel .= "</select>"; //comme dans la vidéo oublie pas de fermer certains balises encore ouverte.
-$formClose = "</form>"; //cloture du form.
-return $sel; 
+$sel .= "</select></form>"; //comme dans la vidéo oublie pas de fermer certains balises encore ouverte.
+return $label . $sel; 
 }
