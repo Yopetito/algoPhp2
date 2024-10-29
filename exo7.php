a59a110c6d39b4a85c4c5a2eac61aa59af8c6fbb
yopetito <yofer.frg@gmail.com>
2024-10-29 21:16:55 +0100

    exo7

--- exo7.php
+++ exo7.php
@@ -9,18 +9,32 @@ genererCheckbox($elements);<br>
 
 <h2>Resultat</h2>
 
-<input type="checkbox" id="choice1" name="choice1" checked />
+<!-- <input type="checkbox" id="choice1" name="choice1" checked />
 <label for="Nom">Nom</label><br>
 <input type="checkbox" id="choice2">
 <label for="Prenom">Prenom</label><br>
 <input type="checkbox" id="choice3">
-<label for="Ville">Ville</label><br>
+<label for="Ville">Ville</label><br> -->
 
 <?php
 
 $elements = [
-    "choix1" => true,
-    "choix2" => false,
-    "choix3" => false];
+    "choix1" => 'checked',
+    "choix2" => '',
+    "choix3" => ''];
 
-function genererCheckbox($elements);
+function genererCheckbox($elements){
+    $htmlSortie = ""; // var stockage, comme dans le 6, stock le HTML dans la var pour apres l'utilisé dans le return.
+    
+    foreach ($elements as $label => $checkedStatus){
+        $checkedAttribute = ($checkedStatus === "checked") ? 'checked' : ''; // Si $checkedStatus(valeur) est "checked", $checkedAttribute prendra la valeur 'checked', sinon elle sera vide
+        // Ajouter la case à cocher HTML à la sortie
+        $htmlSortie .= "<label>";
+        $htmlSortie .= "<input type='checkbox' name='$label' $checkedAttribute> $label";
+        $htmlSortie .= "</label><br>";
+    }
+    
+    return $htmlSortie; // Retourne out le HTML à montré.
+}
+
+echo genererCheckbox($elements);
