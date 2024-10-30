a8cd98a6f39a5632808de7ef38f5b9a983874ba8
Yopetito <Yofer.ruffo@gmail.com>
2024-10-30 13:54:52 +0100

    exo10

---
+++ exo10.php
@@ -0,0 +1,69 @@
+<h1>Exercice 10</h1>
+
+<p>En utilisant l'ensemble des fonctions personnalisées crées précédemment, créer un formulaire<br> 
+complet qui contient les informations suivantes : champs de texte avec nom, prénom, adresse e-mail, ville, sexe et une liste de choix parmi<br>
+lesquels on pourra sélectionner un intitulé de formation :<br> 
+« Développeur Logiciel », « Designer web », « Intégrateur » ou « Chef de projet ».<br>
+Le formulaire devra également comporter un bouton permettant de le soumettre à un traitement<br> 
+de validation (submit)
+</p>
+
+<h2>Resultat</h2>
+
+<?php 
+
+echo afficherform();
+
+function afficherform(){
+$form = "<form action='' method='post'>";
+
+$info = ["Nom", "Prenom", "Adresse e-mail", "Ville"];
+$form .= formTexte($info);
+
+$sexe = ["Masculin", "Feminin", "Autre"];
+$form .= formRadio($sexe);
+
+$job = ["Dév Web", "Intégrateur", "Chef de projet"];
+$form .= formMenuDeroulant($job);
+
+$form .= submitButton();
+
+$form .= "</form>";
+return $form;
+}
+
+function formTexte($array) {
+    $affichage = "";
+    foreach ($array as $value) {
+        $case = "<label for='$value'>$value</label><br>
+                 <input type='text' id='$value' name='$value'><br>";
+        $affichage .= $case;
+    }
+return $affichage;  
+}
+
+function formRadio($array) {
+  $affichage = "Sexe<br>";
+  $name = "genre";
+  foreach ($array as $value) { 
+      $radio = "<input type='radio' id='$value' name='$name' value='$value'>
+                <label for='$value'>$value</label><br>";
+      $affichage .= $radio;
+  }
+return $affichage;
+}
+
+function formMenuDeroulant($array){
+  $label = "<label for='jobs'>Intitulé de formation:</label><br>";
+  $sel = "<select name='jobs' id='jobs'>";
+  foreach ($array as $jobs){
+          $sel .= "<option value='$jobs'>$jobs</option>"; 
+  }
+$sel .= "</select>";
+return $label . $sel; 
+}
+
+function submitButton(){
+  $button = "<input type='submit' value='Submit'><br>";
+return $button;
+}
